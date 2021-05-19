b6ff68109d8a3e8581e90dbe5d8236137a9f2c26
Simon Bosman <s.bosman@speyk.nl>
2021-05-19 14:14:15 +0200

    Retry flow files in compbination with RetryFlow processor.

--- nifi-nar-bundles/nifi-microsoft-graph-bundle/nifi-microsoft-graph-processors/src/main/java/nl/speyk/nifi/microsoft/graph/processors/InvokeMicrosoftGraphCalendar.java
+++ nifi-nar-bundles/nifi-microsoft-graph-bundle/nifi-microsoft-graph-processors/src/main/java/nl/speyk/nifi/microsoft/graph/processors/InvokeMicrosoftGraphCalendar.java
@@ -16,11 +16,15 @@
  */
 package nl.speyk.nifi.microsoft.graph.processors;
 
-import com.google.common.collect.Lists;
-import com.google.gson.*;
+import com.google.common.collect.Lists;import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonParser;
 import com.microsoft.graph.content.BatchRequestContent;
 import com.microsoft.graph.content.BatchResponseContent;
 import com.microsoft.graph.content.BatchResponseStep;
+import com.microsoft.graph.http.GraphErrorResponse;
+import com.microsoft.graph.http.GraphServiceException;
 import com.microsoft.graph.http.HttpMethod;
 import com.microsoft.graph.http.HttpResponseCode;
 import com.microsoft.graph.models.Event;
@@ -62,16 +66,16 @@ import java.util.concurrent.atomic.AtomicReference;
         @WritesAttribute(attribute = "invokeMSGraph.java.exception.class", description = "The Java exception class raised when the processor fails"),
         @WritesAttribute(attribute = "invokeMSGraph.java.exception.message", description = "The Java exception message raised when the processor fails")})
 public class InvokeMicrosoftGraphCalendar extends AbstractProcessor {
-    //Allowed http methods
+    // Allowed http methods
     public final static String GRAPH_METHOD_GET = "GET";
     public final static String GRAPH_METHOD_POST = "POST";
     public final static String GRAPH_METHOD_PATCH = "PATCH";
     public final static String GRAPH_METHOD_DELETE = "DELETE";
 
-    //Microsoft allows max 4 concurent tasks on a mailbox
+    // Microsoft allows max 4 concurent tasks on a mailbox
     public final static int GRAPH_MAILBOX_CONCURRENCY_LIMIT = 4;
 
-    //We want to retry in case of the following errors
+    // We want to retry in case of the following errors
     public final static int GRAPH_HTTP_TO_MANY_REQUESTS = 429;
     public final static int GRAPH_HTTP_SERVICE_UNAVAILABLE = 503;
     public final static int GRAPH_HTTP_GATEWAY_TIMEOUT = 504;
@@ -80,7 +84,7 @@ public class InvokeMicrosoftGraphCalendar extends AbstractProcessor {
     public final static String EXCEPTION_CLASS = "invokeMSGraph.java.exception.class";
     public final static String EXCEPTION_MESSAGE = "invokeMSGraph.java.exception.message";
 
-    //Properties
+    // Properties
     public final static PropertyDescriptor GRAPH_CONTROLLER_ID = new PropertyDescriptor.Builder()
             .name("mg-cs-auth-controller-id")
             .displayName("Graph Controller Service")
@@ -98,7 +102,7 @@ public class InvokeMicrosoftGraphCalendar extends AbstractProcessor {
             .allowableValues(GRAPH_METHOD_GET, GRAPH_METHOD_POST, GRAPH_METHOD_PATCH, GRAPH_METHOD_DELETE)
             .build();
 
-    //relationships
+    // relationships
     public static final Relationship REL_SUCCESS = new Relationship.Builder()
             .name("success")
             .description(
@@ -148,21 +152,20 @@ public class InvokeMicrosoftGraphCalendar extends AbstractProcessor {
             return;
         }
 
-        //Get the controllerservice responsible for an authenticated graphclient
+        // Get the controllerservice responsible for an authenticated graphclient
         MicrosoftGraphCredentialService microsoftGraphCredentialService = context.getProperty(GRAPH_CONTROLLER_ID)
                 .asControllerService(MicrosoftGraphCredentialService.class);
         msGraphClientAtomicRef.set(microsoftGraphCredentialService.getGraphClient());
     }
 
     @Override
-    public void onTrigger(final ProcessContext context, final ProcessSession session) throws ProcessException {
+    public void onTrigger(final ProcessContext context, final ProcessSession session) throws ProcessException, GraphServiceException {
         final ComponentLog logger = getLogger();
         final String httpMethod = context.getProperty(GRAPH_PROP_METHOD).getValue();
 
         FlowFile requestFlowFile = session.get();
 
         if (requestFlowFile == null) {
-            context.yield();
             return;
         }
 
@@ -174,6 +177,11 @@ public class InvokeMicrosoftGraphCalendar extends AbstractProcessor {
         }
 
         try {
+            /**
+             * We expect a flow file with a JsonArray as content
+             * Every JsonObject in the array is an event rewsource type
+             * @see <a href="https://docs.microsoft.com/en-us/graph/api/resources/event?view=graph-rest-1.0"</a>
+             */
             final InputStream inputStream = session.read(requestFlowFile);
             final String eventsJson;
             final Event[] events;
@@ -184,36 +192,38 @@ public class InvokeMicrosoftGraphCalendar extends AbstractProcessor {
             Gson gson = new GsonBuilder().setPrettyPrinting().create();
             JsonElement jsonElement = JsonParser.parseString(eventsJson);
 
-            //In case of a retry we get an Json Object else an Json Array
+            // In case of a retry we get an Json Object else an Json Array
+            // make sure we always have an array of events.
             if (jsonElement.isJsonArray()) {
                 events = gson.fromJson(eventsJson, Event[].class);
             } else if (jsonElement.isJsonObject()) {
                 events = new Event[]{gson.fromJson(eventsJson, Event.class)};
-            }
-            else {
-                logger.error("Not valid JSON.");
+            } else {
+                logger.error("Not valid JSON or empty.");
                 events = new Event[]{};
             }
 
-            //Attributes for success and retry flow files
+            // Attributes for success and retry flow files
             final Map<String, String> attributes = new Hashtable<>();
             attributes.put("Content-Type", "application/json; charset=utf-8");
             attributes.put("mime.type", "application/json");
 
-            //error codes for retry
+            // error codes for retry
             final int[] errorCodes = {GRAPH_HTTP_TO_MANY_REQUESTS, GRAPH_HTTP_SERVICE_UNAVAILABLE, GRAPH_HTTP_GATEWAY_TIMEOUT};
 
-            //Partition the list in sublists of 4 each
+            // Partition the list in sublists of 4 each
             for (List<Event> eventList : Lists.partition(Arrays.asList(events), GRAPH_MAILBOX_CONCURRENCY_LIMIT)) {
-                //JSON batch request
+                // JSON batch request
+                // @see <a href>"https://docs.microsoft.com/en-us/graph/json-batching"</a>
+                final Map<String, Event> idEvent = new Hashtable<>();
                 final BatchRequestContent batchRequestContent = new BatchRequestContent();
 
-                //Four requests per batch
+                // Four requests per batch
                 for (Event event : eventList) {
-                    batchRequestContent.addBatchRequestStep(msGraphClientAtomicRef.get()
+                    idEvent.put(batchRequestContent.addBatchRequestStep(msGraphClientAtomicRef.get()
                             .users(event.organizer.emailAddress.address)
                             .events()
-                            .buildRequest(), HttpMethod.POST, event);
+                            .buildRequest(), HttpMethod.POST, event), event);
                 }
 
                 final BatchResponseContent batchResponseContent = msGraphClientAtomicRef.get()
@@ -221,27 +231,40 @@ public class InvokeMicrosoftGraphCalendar extends AbstractProcessor {
                         .buildRequest()
                         .post(batchRequestContent);
 
-                //Route responses
+                // Route responses
                 for (BatchResponseStep batchResponseStep : batchResponseContent.responses) {
                     if (batchResponseStep.status == HttpResponseCode.HTTP_CREATED) {
+                        //If event is created, put the response in the succes flow file
                         FlowFile succesFlowFile = session.create();
                         succesFlowFile = session.putAllAttributes(succesFlowFile, attributes);
                         session.write(succesFlowFile, out -> IOUtils.write(gson.toJson(batchResponseStep.body), out, StandardCharsets.UTF_8));
                         session.transfer(succesFlowFile, REL_SUCCESS);
-
                     } else if (Arrays.stream(errorCodes).anyMatch(e -> e == batchResponseStep.status)) {
+                        //In case of the following error codes (429, 503, 503)
+                        //put the event from the hashtable in a flow file and route to retry
+                        final Event[] evtRetry = {idEvent.get(batchResponseStep.id)};
+                        String json = gson.toJson(evtRetry, Event[].class);
+
                         FlowFile retryFLowFile = session.create();
                         retryFLowFile = session.putAllAttributes(retryFLowFile, attributes);
-                        session.write(retryFLowFile, out -> IOUtils.write(gson.toJson(batchResponseStep.body), out, StandardCharsets.UTF_8));
+                        //Use the RetryFLow processor for setting the max retries
+                        final String retries;
+                        if ((retries = requestFlowFile.getAttribute("flowfile.retries")) != null) {
+                            retryFLowFile = session.putAttribute(retryFLowFile, "flowfile.retries", retries);
+                        }
+                        else {
+                            retryFLowFile = session.putAttribute(retryFLowFile, "flowfile.retries", "1");
+                        }
+                        session.write(retryFLowFile, out -> IOUtils.write(json, out, StandardCharsets.UTF_8));
                         retryFLowFile = session.penalize(retryFLowFile);
                         session.transfer(retryFLowFile, REL_RETRY);
-
                     } else {
-                        throw new ProcessException(String.format("Microsoft Graph error: %s", batchResponseStep.body));
+                        //This will throw a GraphServiceException or just return null
+                        batchResponseStep.getDeserializedBody(GraphErrorResponse.class);
                     }
                 }
             }
-            //The original flowfile hasn't changed
+            // The original flowfile hasn't changed
             session.transfer(requestFlowFile, REL_ORIGINAL);
 
         } catch (final Exception e) {
